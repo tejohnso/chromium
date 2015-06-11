76b4622f56be63723a5daec57b1375172537cfc3
tejohnso <mail@tyler-johnson.ca>
2015-06-11 01:52:56 -0400

    Ensure webview source can be set to a fs blob

--- tests/chrome-apps/webview_from_blob/main.js
+++ tests/chrome-apps/webview_from_blob/main.js
@@ -7,6 +7,8 @@
     });
 
     retrieveLocalFileBlob()
+    .then(saveToFilesystem)
+    .then(retrieveFilesystemBlob)
     .then(function(resp) {
       var url = URL.createObjectURL(resp);
       console.log("url is " + url + " size " + resp.size);
@@ -15,7 +17,7 @@
     });
   };
 
-  function errH(e) {"Error handler: " + console.log(e.message);}
+  function errH(e) {console.log("Error handler: " + e.message);}
 
   function retrieveLocalFileBlob() {
     return new Promise(function(resolve, reject) {
@@ -28,6 +30,39 @@
       };
 
       xhr.send();
+    });
+  }
+
+  function saveToFilesystem(blob) {
+    return new Promise(function(resolve, reject) {
+      var fs = webkitRequestFileSystem(PERSISTENT, 90000000000, function(fs) {
+        fs.root.getFile("testfs.html", {create: true}, function(fe) {
+          fe.createWriter(function(fw) {
+            fw.onwriteend = function() {
+              resolve();
+            };
+
+            fw.onerror = function(err) {
+              reject(err);
+            };
+
+            fw.write(blob);
+          }, errH);
+        }, errH);
+      }, errH);
+    });
+  }
+
+  function retrieveFilesystemBlob() {
+    return new Promise(function(resolve, reject) {
+      var fs = webkitRequestFileSystem(PERSISTENT, 90000000000, function(fs) {
+        fs.root.getFile("testfs.html", {}, function(fe) {
+          fe.file(function(file) {
+            var reader = new FileReader();
+            resolve(file);
+          });
+        }, errH);
+      }, errH);
     });
   }
 }());
